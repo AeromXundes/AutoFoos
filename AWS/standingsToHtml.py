396474588c3ab1914c0b80c9542ca639c4b0cd94
Kwang Kim <kwang.kim@keysight.com>
2017-10-23 17:46:10 -0400

    temp

--- AWS/standingsToHtml.py
+++ AWS/standingsToHtml.py
@@ -9,7 +9,7 @@ def genHtml(row):
     f.write('<style type="text/css">td{padding:0 15px 0 15px;}</style>')
     f.write('<h2> ' + playerName)
     f.write("<table>")
-    f.write('<tr style="font-weight:bold"><td>Rank</td><td>Overall\t\t</td><td>Offense  </td><td>Defense  </td><td>1 Day Prior   </td><td>1 Week Prior  </td></tr>\n')
+    f.write('<tr style="font-weight:bold"><td>Rank</td><td>Name</td><td>Overall\t\t</td><td>Offense  </td><td>Defense  </td><td>1 Day Prior   </td><td>1 Week Prior  </td></tr>\n')
     table_string = "<tr><td>" + row[4] + "</td><td>" + playerName + "</td><td>" + str(round(float(row[1]), 1)) + "</td><td>" + \
             row[5] + " | " + str(round(float(row[2]), 1)) + '</td><td>' + row[6] + " | " + str(round(float(row[3]), 1)) + '</td><td bgcolor="' + yesColor + '">' +\
             row[8] + '</td><td bgcolor="' + weekColor + '">' + row[12] + "</td></tr>\n"
