9f10f362a9a7035bea50fae0d35d51416f5190d0
Kevin Gibby <kevin.gibby@keysight.com>
2017-10-25 15:10:44 -0400

    Added pigpio code

--- main.ts
+++ main.ts
@@ -87,3 +87,85 @@ var loserOffense = 'Ghi';
 var loserDefense = 'Jkl';
 var loserScore = '4';
 var py = spawn('python', ['RasberryElo.py', winnerOffense,  winnerDefense, loserOffense, loserDefense, loserScore]);
+
+
+// Sensor stuff
+
+// BCM numbering
+const pin_ledYel = 12;
+const pin_irRecvGold = 16;
+const pin_irRecvBlack = 15;
+const pin_ledRed = 7;
+
+var goldScore = 0;
+var blackScore = 0;
+
+var Gpio = require('pigpio').Gpio;
+var ledYel = new Gpio(pin_ledYel, {mode: Gpio.OUTPUT});
+var ledRed = new Gpio(pin_ledRed, {mode: Gpio.OUTPUT});
+var irRecvGold = new Gpio(pin_irRecvGold, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_UP,
+  edge: Gpio.EITHER_EDGE
+});
+var irRecvBlack = new Gpio(pin_irRecvBlack, {
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_UP,
+  edge: Gpio.EITHER_EDGE
+});
+
+function mapIrToLed(channel) {
+  if (channel === pin_irRecvGold) {
+    return pin_ledYel;
+  } else if (channel === pin_irRecvBlack) {
+    return pin_ledRed;
+  } else {
+    return -1;
+  }
+}
+
+function goldBeamConnected(connected: boolean) {
+  if (connected) {
+    ledYel.digitalWrite(1);
+  } else {
+    ledYel.digitalWrite(0);
+  }
+}
+
+function blackBeamConncted(connected: boolean) {
+  if (connected) {
+    ledRed.digitalWrite(1);
+  } else {
+    ledRed.digitalWrite(0);
+  }
+}
+
+function incrementGoldScore() {
+  goldScore += 1;
+  console.log('Gold: ' + goldScore);
+}
+
+function incrementBlackScore() {
+  blackScore += 1;
+  console.log('Black: ' + blackScore);
+}
+
+irRecvBlack.on('interrupt', function(level) {
+  if (level === 1) {
+    // rising edge detected, so beam was connected
+    blackBeamConncted(true);
+  } else {
+    // falling edge detected, so beam was just broken
+    blackBeamConncted(false);
+    incrementBlackScore();
+  }
+});
+
+irRecvGold.on('interrupt', function(level) {
+  if (level === 1) {
+    goldBeamConnected(true);
+  } else {
+    goldBeamConnected(false);
+    incrementGoldScore();
+  }
+});
